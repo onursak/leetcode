54a7449d7d6f17051f2d7929d5ebb39a2d40772f
Onur <33655893+onursak@users.noreply.github.com>
2022-01-20 13:24:20 +0300

    Create LongestSubstringWithoutRepeatingCharacters.java

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,35 @@
+class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0){
+            return 0;
+        }
+        if(s.length() == 1){
+            return 1;
+        }
+        int headPointer = 0;
+        int tailPointer = 0;
+        int maxLength = 0;
+        
+        Map<Character, Integer> chars = new HashMap<>();
+        
+        while(tailPointer < s.length()){
+            char tailChar = s.charAt(tailPointer);
+            if(!chars.containsKey(tailChar)){
+                maxLength = Math.max(maxLength, tailPointer - headPointer + 1);
+                chars.put(tailChar, tailPointer);
+                tailPointer++;
+            }
+            else{
+                int headUpperLimit = chars.get(tailChar) + 1;
+                while(headPointer < headUpperLimit){
+                    chars.remove(s.charAt(headPointer));
+                    headPointer++;
+                }
+                chars.put(tailChar, tailPointer);
+                tailPointer++;
+            }
+        }
+        
+        return maxLength;
+    }
+}
